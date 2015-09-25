d2152e22de4a5b480e88501bf2f70b8aa21339ad
Clément VILLAIN <choclatefr@gmail.com>
2015-09-25 12:26:54 +0200

    Revert "Ml42 user tracking"

--- server/main.py
+++ server/main.py
@@ -2,7 +2,7 @@ import json
 import random
 
 from flask import Flask
-from flask import request, make_response
+from flask import request
 
 import models
 
@@ -16,14 +16,11 @@ def elo(winner_elo, loser_elo):
     loser_elo = loser_elo + K * (0 - p(-D))
     return winner_elo, loser_elo
 
-def update_score(winner, loser, user):
+def update_score(winner, loser):
     winner = models.Session.query(models.Images).get(winner)
     loser = models.Session.query(models.Images).get(loser)
-
     winner.elo, loser.elo = elo(winner.elo, loser.elo)
-    match = models.Matches(user=user, winner=winner, loser=loser)
-
-    models.Session.add_all([winner, loser, match])
+    models.Session.add_all([winner, loser])
     models.Session.commit()
 
 def get_random_images():
@@ -35,25 +32,15 @@ def get_random_images():
 
 @app.route('/back', methods=['GET', 'POST'])
 def back():
-    user_id = request.cookies.get('user_id')
-
-    if not user_id:
-        user = models.Users()
-        models.Session.add(user)
-        models.Session.commit()
-    else:
-        user = models.Session.query(models.Users).get(int(user_id))
-
     if request.method == 'POST' and request.json:
         winner = request.json.get('winner')
         loser = request.json.get('loser')
         if winner and loser:
-            update_score(winner, loser, user.id)
+            update_score(winner, loser)
+        return ''
 
     img1, img2 = get_random_images()
-    response = make_response(json.dumps({'img1': img1, 'img2': img2}))
-    response.set_cookie('user_id', str(user.id))
-    return response
+    return json.dumps({'img1': img1, 'img2': img2})
 
 @app.route('/')
 def index():
